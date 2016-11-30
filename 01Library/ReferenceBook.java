9abb9b7b53acb03291eab502a9e2a893e8e57515
Karen Chen <kchen6600@gmail.com>
2016-11-30 03:35:03 -0500

    updated ReferenceBook.java

---
+++ 01Library/ReferenceBook.java
@@ -0,0 +1,9 @@
+public class ReferenceBook extends LibraryBook{
+    private String collection;
+    
+    public ReferenceBook(String author, String title, String ISBN, String callNumber, String collection){
+	s
+    }
+
+
+}
