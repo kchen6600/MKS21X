8337e2514e3b5ba5c03dd7ff63e6a3dcd1603297
Karen Chen <karenchen@Karens-Air.home>
2016-11-30 20:42:11 -0500

    updated CirculatingBook.java

---
+++ 01Library/CirculatingBook.java
@@ -0,0 +1,51 @@
+public class CirculatingBook extends LibraryBook{
+    private String currentHolder, dueDate;
+    
+    public CirculatingBook(String author, String title, String ISBN, String callNumber){
+	super(author, title, ISBN, callNumber);
+	currentHolder = null;
+	dueDate = null;
+	
+    }
+
+    public String getCurrentHolder(){
+	return currentHolder;
+    }
+
+    public void setCurrentHolder(String currentHolder){
+	this.currentHolder = currentHolder;
+    }
+
+    public String getDueDate(){
+	return dueDate;
+    }
+
+    public void setDueDate(String dueDate){
+	this.dueDate = dueDate;
+    }
+
+    public void checkout(String patron, String due){
+	currentHolder = patron;
+	dueDate = due;
+    }
+
+    public void returned(){
+        currentHolder = null;
+	dueDate = null;
+    }
+
+    public String circulationStatus(){
+	if (currentHolder != null){
+	    return "Current holder: "+currentHolder+"\nDue date: "+dueDate;
+	}
+	else{
+	    return "Book available on shelves.";
+	}
+    }
+
+    public String toString(){
+	return super.toString()+circulationStatus();
+    }
+
+
+}
