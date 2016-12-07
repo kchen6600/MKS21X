614c87adda99fcb9dca9e62fbb366dc65ff6a9fb
Karen Chen <kchen6600@gmail.com>
2016-12-07 03:11:05 -0500

    new file

---
+++ 03Barcode/Barcode.java
@@ -0,0 +1,19 @@
+public class Barcode implements Comparable<Barcode>{
+    private String _zip;
+    private int _checkDigit;
+
+    public Barcode(String zip){
+    }
+    
+    public Barcode clone(){
+    }
+    
+    private int checkSum(){
+    }
+
+    public String toString(){
+    }
+
+    public int compareTo(Barcode other){
+    }
+}
