97a1f3a2022c53c5ffa26aa217a968b3132c35b2
Karen Chen <karenchen@Karens-Air.home>
2016-12-11 00:08:36 -0500

    basic tests

--- 03Barcode/Driver.java
+++ 03Barcode/Driver.java
@@ -1,8 +1,24 @@
 public class Driver{
-    public static void main(String[] args){
-	System.out.println(new Barcode("08451"));
-	System.out.println(Barcode.toCode("08451"));
-	System.out.println(Barcode.toZip("|||:::|::|::|::|:|:|::::|||::|:|"));
-	System.out.println((new Barcode("08451")).compareTo((new Barcode("08451"))));
-    }
+  public static void main(String args[]){
+Barcode b = new Barcode("08451");
+Barcode c = new Barcode("99999");
+Barcode d = new Barcode("01111");
+System.out.println(b); //084518 |||:::|::|::|::|:|:|::::|||::|:|
+System.out.println(b.toString().compareTo("084518 |||:::|::|::|::|:|:|::::|||::|:|")); //0
+System.out.println(b.compareTo(b)); //0
+System.out.println(c.compareTo(b));
+System.out.println(d.compareTo(b));
+/*length
+Barcode e = new Barcode("123456");
+System.out.println(e);
+*/
+/*length
+Barcode e = new Barcode("1234");
+System.out.println(e);
+*/
+/*type
+Barcode e= new Barcode("12.45");
+System.out.println(e);
+*/
+} 
 }
