a7a0affa58e438d14996056a1cb89c2d60b9e55d
Karen Chen <karenchen@Karens-Air.home>
2016-12-14 21:21:02 -0500

    updated includes basic tests

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -4,5 +4,60 @@ public class Sorts{
     }
 
     public static void selectionSort(int[] data){
+	int min;
+	int temp;
+	int index;
+
+	for (int i = 0; i < data.length-1; i++){
+	    min = data[i];
+	    index = i;
+	    for (int j = i+1; j < data.length; j++){
+		if (data[j] < min){
+		    min = data[j];
+		    index = j;
+		}
+	    }
+	    temp = data[i];
+	    data[i] = min;
+	    data[index] = temp;
+	}
+	    
     }
+
+    private static void insertionSort(int[] data){
+
+	int temp;
+	int saved;
+	for (int index = 1; index < data.length; index++){
+	    temp = data[index];
+	    saved = index;
+	    while (saved > 0 && temp < data[saved - 1]){
+		data[saved] = data[saved - 1];
+		saved -= 1;
+	    }
+	    data[saved] = temp;
+	}			
+    }
+
+    private static String arrayToString(int[]data){
+	String str = "[";
+	
+	for (int i = 0; i < data.length; i++){
+	    str += data[i] + ", ";
+	}
+
+	str += "]";
+	return str;
+    }
+
+    public static void main(String[] args){
+	int[] test = {62, 25, 12, 22, 11};
+	selectionSort(test);
+	System.out.println(arrayToString(test));
+	int[] test2 = {0, 1, 9, 7, 2};
+	insertionSort(test2);
+	System.out.println(arrayToString(test2));
+    }
+	
+
 }
